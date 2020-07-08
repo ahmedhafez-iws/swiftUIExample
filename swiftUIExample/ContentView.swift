d1fe4e50bbdbd2b9279b39f19939d7c1d76ea92c
ahmedhafez-iws <53041696+ahmedhafez-iws@users.noreply.github.com>
2020-07-08 10:34:44 +0200

    Initial Commit

---
+++ swiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIExample
+//
+//  Created by user174699 on 7/8/20.
+//  Copyright © 2020 user174699. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
